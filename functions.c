0508051eb4b98b5b3287ac52c27b57e643f89f47
RicardoDanta <uruguaydenos@gmail.com>
2022-08-16 12:21:40 -0700

    0

--- functions.c
+++ functions.c
@@ -5,7 +5,7 @@
  * @queue: Something
  * @num: Something
  */
-void push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int)
 {
 	stack_t *newNode;
 	int num = 0;
@@ -20,7 +20,7 @@ void push(stack_t **stack, unsigned int line_number)
 		(*stack)->prev = newNode;
 	(*stack) = newNode;
 }
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack, unsigned int)
 {
 	stack_t *valnode = *stack;
 
