8f7e93250e1e9b40807bfcdfe70e03339437d8ea
RicardoDanta <uruguaydenos@gmail.com>
2022-08-17 12:58:06 -0700

    Changes

--- functions.c
+++ functions.c
@@ -8,15 +8,15 @@
 void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *newNode;
-	int line_number = 0;
+	int number = 0;
 
 	newNode = malloc(sizeof(stack_t));
 
-	newNode->n = line_number;
+	newNode->n = number;
 	newNode->next = *stack;
 	newNode->prev = NULL;
 
-	if ((*stack) !=NULL)	
+	if ((*stack) != NULL)	
 		(*stack)->prev = newNode;
 	(*stack) = newNode;
 }
@@ -24,7 +24,7 @@ void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *valnode = *stack;
 
-	while (valnode !=NULL)
+	while (valnode != NULL)
 	{
 		printf("%d/n", valnode->n);
 	}
