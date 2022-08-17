dd2499a280547148b116ffd92ef3612f77e8a7a4
RicardoDanta <uruguaydenos@gmail.com>
2022-08-17 12:54:12 -0700

    Changes

--- functions.c
+++ functions.c
@@ -3,16 +3,16 @@
 /**
  * push - function that pushes an element to the stack
  * @queue: Something
- * @num: Something
+ * @line_number: Something
  */
-void push(stack_t **stack, unsigned int)
+void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *newNode;
-	int num = 0;
+	int line_number = 0;
 
 	newNode = malloc(sizeof(stack_t));
 
-	newNode->n = num;
+	newNode->n = line_number;
 	newNode->next = *stack;
 	newNode->prev = NULL;
 
@@ -20,7 +20,7 @@ void push(stack_t **stack, unsigned int)
 		(*stack)->prev = newNode;
 	(*stack) = newNode;
 }
-void pall(stack_t **stack, unsigned int)
+void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *valnode = *stack;
 
---
+++ monty.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ *
+ */
+int main(int argcounter, char *arglist)
+{
+	FILE *file;
+
+	file = fopen(arglist[1], "r");
+	return(EXIT-SUCCESS);
+}
